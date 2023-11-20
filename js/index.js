4a20f569cddd660f2f0a4ff8eef742e25d3a9e09
Myroslava Snesar <137907987+MirraSnesar@users.noreply.github.com>
2023-11-20 20:22:37 +0000

    added new in shop part

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 document.addEventListener ('partialsLoaded', () => {
     import('./testimonials.js');
+    import('./new-in-shop.js');
+    import('./products-catalogue.js');
+    import('./products-service.js');
 });
---
+++ js/products-catalogue.js
@@ -0,0 +1,145 @@
+const daphne = {
+    name: 'Daphne',
+    composition: 'roses, lilies, celosias, flowering kale',
+    NewInShop: true,
+    code: '21',
+    price: 59,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/daphne.png'],
+    description: "This exquisite bouquet will not fade for a long time and will delight the eye with its presence.",
+}
+  
+const autumn = {
+    name: 'Autumn',
+    composition: 'giant lily, chrysanthemums, dahlias, pansies',
+    NewInShop: true,
+    code: '20',
+    price: 69,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/autumn.png'],
+    description: "A beautiful bouquet in autumn colors will remind the recipient of you and say hello from you to your friends, family members, colleagues, or acquaintances.",
+}
+  
+const fantasy = {
+    name: 'Fantasy',
+    composition: 'chrysanthemums, dandelions, forget-me-nots, alstroemerias',
+    NewInShop: true,
+    code: '22',
+    price: 119,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/fantasy.png'],
+    description: "This wonderful combination of different flowers, textures, and colors can be a great bouquet for any occasion.",
+}
+  
+const amphora = {
+    name: 'Amphora',
+    composition: 'red and yellow roses, chrysanthemums',
+    NewInShop: true,
+    code: '25',
+    price: 72,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/amphora.png'],
+    description: "This beautiful bouquet is the perfect way to show your love and appreciation. It creates a brilliant blend of the traditional and the modern.",
+}
+  
+const josephine = {
+    name: 'Josephine',
+    composition: 'roses, dahlias, daisies, ferns',
+    NewInShop: true,
+    code: '19',
+    price: 99,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/josephine.png'],
+    description: "This elegant and luxurious bouquet consists of several types of flowers and is perfect for expressing gratitude.",
+}
+  
+const poetry = {
+    name: 'Poetry',
+    composition: 'roses, dahlias, daisies, ferns',
+    NewInShop: true,
+    code: '24',
+    price: 59,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/poetry.png'],
+    description: "Delicate and light bouquet for everyone who loves pastel color combinations and elegant fragrances.",
+}
+  
+const grace = {
+    name: 'Grace',
+    composition: 'roses, dahlias, daisies, ferns',
+    NewInShop: true,
+    code: '8',
+    price: 59,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/grace.png'],
+    description: "This beautiful bouquet will convey a warm hello from you to your loved ones and fill their home with an elegant fragrance.",
+}
+  
+const innocence = {
+    name: 'Innocence',
+    composition: 'lilies, field daisies, roses, fern',
+    NewInShop: true,
+    code: '27',
+    price: 134,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/innocence.png'],
+    description: "This gorgeous all-white bouquet is a stunning tribute to the purity of love. Lilies represent innocence and humility, while daisies symbolize cheerfulness and optimism. The roses complete the bouquet with a hint of timeless romance.",
+}
+const sparkle = {
+    name: 'Sparkle',
+    composition: 'chrysanthemums, alstroemerias, lilies, fern',
+    NewInShop: true,
+    code: '28',
+    price: 80,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/sparkle.png'],
+    description: "This stunning bouquet is a vibrant tapestry of color and texture. The lush array of blooms includes chrysanthemums in shades of yellow and rust, delicate lilies, and cheerful alstroemerias in pink and peach. Perfect for any special occasion, this gorgeous bouquet is sure to make someone smile.",
+}
+const ginebra = {
+    name: 'Ginebra',
+    composition: 'lilies, daisies, roses, alstroemerias',
+    NewInShop: true,
+    code: '26',
+    price: 98,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/ginebra.png'],
+    description: "This stunning bouquet is a vibrant tapestry of color and texture. The lush array of blooms includes chrysanthemums in shades of yellow and rust, delicate lilies, and cheerful alstroemerias in pink and peach. Perfect for any special occasion, this gorgeous bouquet is sure to make someone smile.A combination of long-stemmed white lilies and cheerful daisies in shades of blue come together in an explosion of color and fragrance, perfect for any occasion!",
+}
+const ellie = {
+    name: 'Ellie',
+    composition: 'corn poppies, daisies, cornflowers, blanket flowers',
+    NewInShop: true,
+    code: '18',
+    price: 59,
+    currency: 'USD',
+    inStock: true,
+    images: ['img/new-in-shop/ellie.png'],
+    description: "Fresh and delicate, like a summer day in a flowery meadow, this bouquet is perfect for any occasion.",
+}
+
+const products = [
+    daphne,
+    autumn,
+    fantasy,
+    amphora,
+    josephine,
+    poetry,
+    grace,
+    innocence,
+    sparkle,
+    ginebra,
+    ellie,
+]
+  
+export { products }
+  
