b9037d63776bc764547badddab72e9fec7b029c9
Myroslava Snesar <137907987+MirraSnesar@users.noreply.github.com>
2023-11-24 21:11:22 +0000

    added 3 new sections

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ document.addEventListener ('partialsLoaded', () => {
     import('./products-catalogue.js');
     import('./header.js');
     import('./burger.js');
+    import('./products-service.js');
     import('./footer.js');
-
+    import('./contact-form.js');
+    import('./cart.js');
+    import('./cart-badge.js');
+    import('./shop-list.js');
+    import('./store-home.js');
 });
